8eb439c82d91816da4a87b083242dcba2ed7451b
Nathan Hall <nhall@trhc.com>
2019-02-23 15:32:10 -0500

    Update index

--- api/index.js
+++ api/index.js
@@ -6,9 +6,21 @@ const app = express()
 app.use(bodyParser.json())
 
 app.post('/api/webhook', (req, res) => {
-  const { subject, question, tuteePhoneNumber } = req.body
+  if (
+    req.body.queryResult.fulfillmentText ===
+    "Cool. Now that I know your age and the subject you want to learn, I'll have a tutor contact you shortly."
+  ) {
+    const { Subject, Age } = req.body.queryResult.parameters
+    const phoneNumber = req.body.originalDetectIntentRequest.payload.data.To
 
-  res.status(200).json({ subject, question, tuteePhoneNumber })
+    console.log('subject: ', Subject)
+    console.log('Age: ', Age)
+    console.log('Phone Number: ', phoneNumber)
+    res.status(200).json({ Subject, Age, phoneNumber })
+  } else {
+    console.log('NO NEED TO SEND')
+    res.status(418).json({ message: 'I AM TEAPOT' })
+  }
 })
 
 app.listen(PORT, () => {
