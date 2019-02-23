6db0aceafc923986e85746276f72389b114173cb
Calvin Webster <calweb@gmail.com>
2019-02-23 16:07:56 -0500

    moar codezzzz

--- api/index.js
+++ api/index.js
@@ -3,9 +3,14 @@ const bodyParser = require('body-parser')
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const PORT = process.env.PORT || 8080
 const app = express()
+const { createStudent, startConversation} = require('./lib/actions')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.get('/', (req, res) => {
+  console.log(process.env.TWILIO_ACCOUNT_SID)
+  res.json({message: "You have reached the server"})
+})
 app.post('/api/webhook', (req, res) => {
   if (
     req.body.queryResult.fulfillmentText ===
@@ -13,10 +18,13 @@ app.post('/api/webhook', (req, res) => {
   ) {
     const { Subject, Age } = req.body.queryResult.parameters
     const phoneNumber = req.body.originalDetectIntentRequest.payload.data.To
-
+    startConversation({student: phoneNumber, tutor: '+18432597827', subject: Subject}).then(responseFromAirTable => {
+      console.log('RESPONSE FROM AIRTABLE >>>> ', responseFromAirTable)
+    })
     console.log('subject: ', Subject)
     console.log('Age: ', Age)
     console.log('Phone Number: ', phoneNumber)
+
     res.status(200).json({ Subject, Age, phoneNumber })
   } else {
     console.log('NO NEED TO SEND')
