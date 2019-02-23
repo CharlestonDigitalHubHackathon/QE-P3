4d2c8f7307f901a36e80dd18e96397ac6b1cd31c
Travis Nesland <tnesland@gmail.com>
2019-02-23 15:28:11 -0500

    Add action: startConversation

--- api/actions.js
+++ api/actions.js
@@ -3,11 +3,20 @@ const db = require('./clients/airtable')
 // All of these actions should return a promise
 module.exports = {
   createStudent,
+  startConversation,
 }
 
 function createStudent({ age, phoneNumber }) {
   return db('students').create({
     age: age,
     phone_number: phoneNumber
+  })
+}
+
+function startConversation({ student, tutor, subject }) {
+  return db('conversations').create({
+    student,
+    subject,
+    tutor,
   })
 }
