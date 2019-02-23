198daaba9c0e945162f6d2ec3bbdc9add94ec4b1
Travis Nesland <tnesland@gmail.com>
2019-02-23 16:19:15 -0500

    Add action: createEvent
    Add action to take a plain old object and save it in events table

--- api/lib/actions.js
+++ api/lib/actions.js
@@ -2,6 +2,7 @@ const db = require('../clients/airtable')
 
 // All of these actions should return a promise
 module.exports = {
+  createEvent,
   createStudent,
   startConversation,
 }
@@ -10,6 +11,17 @@ function createStudent({ age, phoneNumber }) {
   return db('students').create({
     age: age,
     phone_number: phoneNumber
+  })
+}
+
+/**
+ * create and store an event in events table
+ *
+ * data - plain old json object (will be converted to json)
+ */
+function createEvent(data) {
+  return db('events').create({
+    data: JSON.stringify(data)
   })
 }
 
